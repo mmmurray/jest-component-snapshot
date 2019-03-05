c2d91e448578b7d9e1a1d5a5e36f003ee97ee8ac
Mark Murray <mark@murray.xyz>
2019-03-05 20:50:02 +0000

    connect to existing puppeteer if exists

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -1,7 +1,12 @@
 import puppeteer from 'puppeteer'
 
 const loadPage = async () => {
-  const browser = await puppeteer.launch()
+  const browser = await (process.env.PUPPETEER_WS_ENDPOINT
+    ? puppeteer.connect({
+        browserWSEndpoint: process.env.PUPPETEER_WS_ENDPOINT,
+      })
+    : puppeteer.launch())
+
   const page = await browser.newPage()
 
   const teardown = async () => {
