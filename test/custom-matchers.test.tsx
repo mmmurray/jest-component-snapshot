0c9e187f571508d6dbc4c0473f6bd23e1ed813af
Mark Murray <mark@murray.xyz>
2019-03-06 17:22:47 +0000

    merge tests

--- test/custom-matchers.test.tsx
+++ test/custom-matchers.test.tsx
@@ -1,49 +1,65 @@
+import styled from '@emotion/styled'
 import React from 'react'
 import { extendExpect } from '../src'
 import 'jest-component-snapshot'
 
 extendExpect()
 
+const stringToDomElement = html => {
+  const div = document.createElement('div')
+  div.innerHTML = html
+
+  return div.firstChild
+}
+
+const StyledHeader = styled.header`
+  padding: 12px;
+  border-radius: 4px;
+  background-color: lime;
+  font-family: sans-serif;
+`
+
+const htmlStringElement = '<header><h1>Hello</h1><h2>World</h2></header>'
+const domElement = stringToDomElement(htmlStringElement)
+const reactElement = (
+  <StyledHeader>
+    <h1>Hello</h1>
+    <h2>World</h2>
+  </StyledHeader>
+)
+
 test('can use toMatchA11ySnapshot with HTML string', async () => {
-  await expect(
-    '<header><h1>Hello</h1><h2>World</h2></header>',
-  ).toMatchA11ySnapshot()
+  await expect(htmlStringElement).toMatchA11ySnapshot()
 })
 
 test('can use toMatchA11ySnapshot with DOM element', async () => {
-  const element = document.createElement('h1')
-  element.innerHTML = 'Hello'
-
-  await expect(element).toMatchA11ySnapshot()
+  await expect(domElement).toMatchA11ySnapshot()
 })
 
 test('can use toMatchA11ySnapshot with React element', async () => {
-  await expect(
-    <header>
-      <h1>Hello</h1>
-      <h2>World</h2>
-    </header>,
-  ).toMatchA11ySnapshot()
+  await expect(reactElement).toMatchA11ySnapshot()
 })
 
 test('can use toMatchDomSnapshot with HTML string', async () => {
-  await expect(
-    '<header><h1>Hello</h1><h2>World</h2></header>',
-  ).toMatchDomSnapshot()
+  await expect(htmlStringElement).toMatchDomSnapshot()
 })
 
 test('can use toMatchDomSnapshot with DOM element', async () => {
-  const element = document.createElement('h1')
-  element.innerHTML = 'Hello'
-
-  await expect(element).toMatchDomSnapshot()
+  await expect(domElement).toMatchDomSnapshot()
 })
 
 test('can use toMatchDomSnapshot with React element', async () => {
-  await expect(
-    <header>
-      <h1>Hello</h1>
-      <h2>World</h2>
-    </header>,
-  ).toMatchDomSnapshot()
+  await expect(reactElement).toMatchDomSnapshot()
+})
+
+test('can use toMatchImageSnapshot with HTML string', async () => {
+  await expect(htmlStringElement).toMatchImageSnapshot()
+})
+
+test('can use toMatchImageSnapshot with DOM element', async () => {
+  await expect(domElement).toMatchImageSnapshot()
+})
+
+test('can use toMatchImageSnapshot with React element', async () => {
+  await expect(reactElement).toMatchImageSnapshot()
 })
